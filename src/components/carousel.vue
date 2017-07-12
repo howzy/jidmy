360a1d9e86f766a93352e566ed9a9165a5ea3c9a
once <zhonce829@gmail.com>
2017-07-12 17:44:37 +0800

    添加首屏轮播图

---
+++ src/components/carousel.vue
@@ -0,0 +1,43 @@
+<template>
+  <swiper :options="swiperOption" ref="mySwiper">
+    <!-- slides -->
+    <swiper-slide><a href=""><img src="../assets/img/bg1.jpg" alt="轮播图"></a></swiper-slide>
+    <swiper-slide><a href=""><img src="../assets/img/bg2.jpg" alt="轮播图"></a></swiper-slide>
+    <!-- Optional controls -->
+     <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+</template>
+
+<script>
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+
+export default {
+  data () {
+    return {
+      swiperOption: {
+        notNextTick: true,
+        autoplay: 3000,
+        loop: true,
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        effect: 'fade'
+      }
+    }
+  },
+  components: {
+    swiper,
+    swiperSlide
+  }
+}
+</script>
+
+<style lang="scss">
+.swiper-slide {
+  width: 100%;
+  height: 850px;
+  a {
+    display: flex;
+    justify-content: center;
+  }
+}
+</style>
--- src/page/home.vue
+++ src/page/home.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="home">
+    <carousel></carousel>
     <h1>吉鼎主页</h1>
   </div>
 </template>
+
+<script>
+import carousel from '../components/carousel'
+
+export default {
+  components: {
+    carousel
+  }
+}
+</script>
