5a82c539004b023d3f9991b4d54149c1a5a6fd2d
once <zhonce829@gmail.com>
2017-07-17 17:13:34 +0800

    添加关于礼享模块

---
+++ src/page/about.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="about">
+    <div class="about_bg"></div>
+    <div class="container">
+      <h3>企业文化</h3>
+      <p class="title">一、思想体系</p>
+      <p class="sub-title">1、核心理念</p>
+      <p class="content">吉鼎企业文化的核心理念是“国际吉鼎，百年企业”。“国际吉鼎”就是企业规模、管理、文化达到国际级，成为世界一流企业；“百年企业”是指企业要追求基业长青，追求长远利益，打造可持续发展的家庭消费返礼新平台。</p>
+      <p class="sub-title">2、企业使命</p>
+      <p class="content">“共创财富，公益社会</p>
+      <p class="sub-title">3、核心价值观</p>
+      <p class="content">人的价值高于物的价值，企业价值高于员工个人价值，社会价值高于企业价值。</p>
+      <p class="title">二、制度体系</p>
+      <p class="content">吉鼎为了保证企业文化的传承、传播、贯彻、执行，实行360度服务客户流程，并保证做到：加盟有计划，联盟有保障；经费有预算，培训有考核。</p>
+      <p class="title">三、主要特点</p>
+      <p class="sub-title">1、勇敢创新</p>
+      <p class="content">敢于创新是吉鼎文化的首要特点，就是敢闯敢试、敢想敢干。吉鼎是全国首家进行礼品经营商业模式创新的企业；吉鼎也是实现礼品线下线上跨区域发展的企业。</p>
+      <p class="sub-title">2、坚守诚信</p>
+      <p class="content">坚守诚信是吉鼎文化的核心特点。</p>
+      <p class="sub-title">3、精诚团结</p>
+      <p class="content">精诚合作，团结一致是吉鼎的团队精神。</p>
+      <p class="sub-title">4、多边共赢</p>
+      <p class="content">吉鼎的企业信仰始终是多边共赢，让我们的合作伙伴获利，让选择我们的用户获利。</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+  .container {
+    width: 1220px;
+    margin: 0 auto;
+    padding: 40px 0;
+  }
+  .about {
+    .about_bg {
+      width: 100%;
+      height: 645px;
+      background: url('../assets/img/about_bg.jpg') center top no-repeat;
+    }
+  }
+  h3 {
+    font-size: 24px;
+  }
+  p {
+    font-size: 16px;
+    margin-top: 16px;
+    &.sub-title {
+      font-weight: 400;
+      text-indent: 20px;
+    }
+    &.content {
+      line-height: 2;
+      text-indent: 50px;
+    }
+  }
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import home from '../page/home'
 import service from '../page/service'
+import about from '../page/about'
 
 Vue.use(Router)
 
@@ -11,6 +12,10 @@ export default new Router({
       path: '/',
       name: 'home',
       component: home
+    },
+    {
+      path: '/about',
+      component: about
     },
     {
       path: '/service',
