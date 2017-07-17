e44cc549c238e187178a7c60df186bd42b1e6f7b
once <zhonce829@gmail.com>
2017-07-17 15:34:04 +0800

    添加服务中心

---
+++ src/page/service.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="service">
+    <div class="bg"></div>
+    <div class="content">
+      <p><img src="../assets/img/service_content.png"></p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+  .service {
+    width:100%;
+    overflow: hidden;
+    .bg {
+      width: 100%;
+      height: 645px;
+      background: url('../assets/img/service_bg.png') center top no-repeat;
+    }
+    .content {
+      width: 970px;
+      margin: 0 auto;
+    }
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import home from '../page/home'
+import service from '../page/service'
 
 Vue.use(Router)
 
@@ -10,6 +11,10 @@ export default new Router({
       path: '/',
       name: 'home',
       component: home
+    },
+    {
+      path: '/service',
+      component: service
     }
   ]
 })
