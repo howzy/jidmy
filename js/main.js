dd86b71e4b836c762da985691e9e413a6e2cdaf4
howzy <ihowzy@gmail.com>
2017-08-08 23:54:32 +0800

    企业实力

--- js/main.js
+++ js/main.js
@@ -2,11 +2,20 @@
   new WOW().init();
   
   // 滚动切换顶部导航栏状态
+  var count = 0;
   $(window).scroll(function () {
     if ($(".navbar").offset().top > 50) {
       $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
       $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+    // 首次滚动至该位置触发
+    if ($(document).scrollTop() > $('#count').offset().top - $(window).innerHeight() && !count) {
+      count = 1;
+      numberCountUp("count-1", 20);
+      numberCountUp("count-2", 100);
+      numberCountUp("count-3", 10000);
+      numberCountUp("count-4", 500);
     }
   });
 
@@ -17,4 +26,16 @@
     pagination: '.swiper-pagination',
     paginationClickable: true
   });
+
+  // 数字增加动画
+  function numberCountUp(id, value) {
+    var options = {
+      useEasing : false, // 缓动效果
+      useGrouping : false, // 是否每千位分隔，例如：1,000,000
+      separator : ',', // 分隔符
+      suffix: '+' // 后缀
+    };
+    // 参数含义：动画元素id，初始值，最终值，小数位，动画持续时间，参数
+    new CountUp(id, 0, value, 0, 2, options).start();
+  }
 })(jQuery);
