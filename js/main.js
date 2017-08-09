f1dab3cc25d45c9a32c577142b33d36fe0df7fe6
once <zhonce829@gmail.com>
2017-08-09 14:28:22 +0800

    媒体报道

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 (function ($) {
   new WOW().init();
-  
+
   // 滚动切换顶部导航栏状态
   var count = 0;
   $(window).scroll(function () {
@@ -30,12 +30,30 @@
   // 数字增加动画
   function numberCountUp(id, value) {
     var options = {
-      useEasing : false, // 缓动效果
-      useGrouping : false, // 是否每千位分隔，例如：1,000,000
-      separator : ',', // 分隔符
+      useEasing: false, // 缓动效果
+      useGrouping: false, // 是否每千位分隔，例如：1,000,000
+      separator: ',', // 分隔符
       suffix: '+' // 后缀
     };
     // 参数含义：动画元素id，初始值，最终值，小数位，动画持续时间，参数
     new CountUp(id, 0, value, 0, 2, options).start();
   }
+
+  // 滚动至锚点
+  $(function () {
+    $('.navbar-nav li a').bind('click', function (event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+      }, 1500, 'easeInOutExpo');
+      event.preventDefault();
+    });
+    $('.page-scroll a').bind('click', function (event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+      }, 1500, 'easeInOutExpo');
+      event.preventDefault();
+    });
+  });
 })(jQuery);
